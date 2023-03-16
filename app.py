d6ae15c079ebf5f8d0d7f41594be55ff6c75f10c
blongo <basilelongo@outlook.fr>
2023-03-16 15:12:18 +0100

    aruco added

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 face_cascade = cv2.CascadeClassifier()
 face_cascade.load(cv2.samples.findFile("static/haarcascade_frontalface_alt.xml"))
 
+dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_5X5_50)
+parameters =  cv2.aruco.DetectorParameters()
+detector = cv2.aruco.ArucoDetector(dictionary, parameters)
+
 @app.route('/')
 def index():
     return "Default Message"
@@ -14,8 +18,37 @@ def gen(video):
         success, image = video.read()
         frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         frame_gray = cv2.equalizeHist(frame_gray)
-
+        (corners, ids, rejected) = detector.detectMarkers(image)
         faces = face_cascade.detectMultiScale(frame_gray)
+        if len(corners) > 0:
+            # flatten the ArUco IDs list
+            ids = ids.flatten()
+            # loop over the detected ArUCo corners
+            for (markerCorner, markerID) in zip(corners, ids):
+                # extract the marker corners (which are always returned in
+                # top-left, top-right, bottom-right, and bottom-left order)
+                corners = markerCorner.reshape((4, 2))
+                (topLeft, topRight, bottomRight, bottomLeft) = corners
+                # convert each of the (x, y)-coordinate pairs to integers
+                topRight = (int(topRight[0]), int(topRight[1]))
+                bottomRight = (int(bottomRight[0]), int(bottomRight[1]))
+                bottomLeft = (int(bottomLeft[0]), int(bottomLeft[1]))
+                topLeft = (int(topLeft[0]), int(topLeft[1]))
+                			# draw the bounding box of the ArUCo detection
+                cv2.line(image, topLeft, topRight, (0, 255, 0), 2)
+                cv2.line(image, topRight, bottomRight, (0, 255, 0), 2)
+                cv2.line(image, bottomRight, bottomLeft, (0, 255, 0), 2)
+                cv2.line(image, bottomLeft, topLeft, (0, 255, 0), 2)
+                # compute and draw the center (x, y)-coordinates of the
+                # ArUco marker
+                cX = int((topLeft[0] + bottomRight[0]) / 2.0)
+                cY = int((topLeft[1] + bottomRight[1]) / 2.0)
+                cv2.circle(image, (cX, cY), 4, (0, 0, 255), -1)
+                # draw the ArUco marker ID on the frame
+                cv2.putText(image, str(markerID),
+                    (topLeft[0], topLeft[1] - 15),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.5, (0, 255, 0), 2)
 
         for (x, y, w, h) in faces:
             center = (x + w//2, y + h//2)
